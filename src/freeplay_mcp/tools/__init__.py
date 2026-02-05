5be270af08fce90a0f0eb61c2169aeaf43be47f1
BrianNewsom <brian.l.newsom@gmail.com>
2026-02-05 10:33:14 -0700

    Remove deploy from create and deploy step of prompt

--- src/freeplay_mcp/tools/__init__.py
+++ src/freeplay_mcp/tools/__init__.py
@@ -7,7 +7,7 @@ from .projects import list_projects
 from .completions import search_completions
 from .traces import search_traces
 from .sessions import search_sessions
-from .prompts import list_prompt_templates, get_prompt_version, create_prompt_version_and_deploy
+from .prompts import list_prompt_templates, get_prompt_version, create_prompt_version, get_deployed_prompts
 from .logging_analysis import find_logging_issues
 from .prompt_optimization import optimize_prompt
 from .insights import list_insights
@@ -19,7 +19,8 @@ __all__ = [
     "search_sessions",
     "list_prompt_templates",
     "get_prompt_version",
-    "create_prompt_version_and_deploy",
+    "create_prompt_version",
+    "get_deployed_prompts",
     "find_logging_issues",
     "optimize_prompt",
     "list_insights",
